b4c89fc374e5469f88833cde5cd5c241e28d5a44
kimminseok <kms3288381@naver.com>
2022-09-24 15:18:07 +0900

    알고리즘 문제 1,2번

---
+++ 2.짝수 홀수.js
@@ -0,0 +1,15 @@
+//변수 num 지정해주기
+//if문 사용해서 홀수일 경우 짝수일 경우
+//홀수 -> odd, 짝수는 -> even
+
+
+function num(x){
+    if(x%2==0){
+        return('Even')
+        }else if (x%2!=0){
+            return('solution')
+        }
+    }
+
+let y = num(11)
+console.log(y)
