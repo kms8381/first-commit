86d187d7bdaf3fe98d73b182b02c20759584744e
kimminseok <kms3288381@naver.com>
2022-09-27 23:12:21 +0900

    풀이 수정

--- 7.음양 더하기.js
+++ 7.음양 더하기.js
@@ -5,14 +5,14 @@
 //for문으로 반복문 작성 후 if문 으로 조건문
 
 function solution(absolutes, signs) {
-    let sum=0; // 더하거나 뺀 값을 저장해줄 함수 지정
+    let sum=0; // 더하거나 뺀 값을 저장해 함수 지정
     for (i=0; i<absolutes.length; i++){ // absolutes의 길이만큼 반복해줄 for문
         if (signs[i]==true){ //signs의 i번째 값이 true라면 
             sum+=absolutes[i];//sum에 absolutes의i번째 숫자를 더해라
         }else if(signs[i]==false){//false라면
             sum-=absolutes[i]  //뺀 값을 저장해라
         }
-    }
+    }줄
     return(sum)//sum에 저장된 값을 반환해준다.
 }
 
