786e9fff7f063a8db7082a4e64f8bf6cbd3bf9a6
kimminseok <kms3288381@naver.com>
2022-11-30 23:18:22 +0900

    프로그래머스 32번 문제

---
+++ .vscode/32.두개 뽑아서 더하기.js
@@ -0,0 +1,12 @@
+function solution(numbers) {
+  let answer = [];
+  for (let i = 0; i < numbers.length; i++) {
+    for (let j = 0; j < numbers.length; j++) {
+      if (i === j) continue;
+      answer.push(numbers[i] + numbers[j]);
+    }
+  }
+  answer = [...new Set(answer)].sort((a, b) => a - b);
+  console.log(answer);
+  return answer;
+}
